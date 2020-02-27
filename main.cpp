c4d163e016455d6c3f89bb0ebc6774e50e1bf211
kristo <kristo@kvb2.kvb2>
2020-02-26 22:52:53 -0600

    metodos de insercion, shell sort y seleccion añadidos

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ enum ARG
 };
 
 void burbuja(int n, unsigned long long arr[]);
+void insercion(int n, unsigned long long arr[]);
+void shell_sort(int n, unsigned long long arr[]);
+void seleccion(int n, unsigned long long arr[]);
 void crear_datos(int n, unsigned long long arr[]);
 
 int main(int argc, char* argv[])
@@ -22,8 +25,7 @@ int main(int argc, char* argv[])
     srand(time(NULL));
 
     crear_datos(n, arr);
-    burbuja(n, arr);
-    burbuja(n, arr);
+    seleccion(n, arr);
 
     return 0;
 }
@@ -61,4 +63,83 @@ void burbuja(int n, unsigned long long arr[])
     for(int i = 0; i < n; i++)
         archivo << arr[i] << endl;
     archivo << "---------------------------------------------\n";
+}
+
+void insercion(int n, unsigned long long arr[])
+{
+    unsigned long long tmp;
+    string nombre = "resultado_insercion_";
+    nombre += to_string(n) + ".txt";
+    int j;
+
+    for (int i = 1; i < n; i++)
+    {
+        tmp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > tmp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = tmp;
+    }
+
+    fstream archivo(nombre, ios::out | ios::app);
+    for(int i = 0; i < n; i++)
+        archivo << arr[i] << endl;
+    archivo << "---------------------------------------------\n";
+}
+
+void shell_sort(int n, unsigned long long arr[])
+{
+    unsigned long long tmp;
+    string nombre = "resultado_shell_sort_";
+    nombre += to_string(n) + ".txt";
+
+    int brecha = n / 2;
+    int j;
+    while (brecha > 0)
+    {
+        for (int i = brecha; i < n; ++i)
+        {
+            tmp = arr[i];
+            j = i;
+            while (j >= brecha && arr[j - brecha] > tmp)
+            {
+                arr[j] = arr[j - brecha];
+                j -= brecha;
+            }
+            arr[j] = tmp;
+        }
+        brecha /= 2;
+    }
+
+    fstream archivo(nombre, ios::out | ios::app);
+    for(int i = 0; i < n; i++)
+        archivo << arr[i] << endl;
+    archivo << "---------------------------------\n";
+}
+
+void seleccion(int n, unsigned long long arr[])
+{
+    unsigned long long tmp;
+    string nombre = "resultado_seleccion_";
+    nombre += to_string(n) + ".txt";
+
+    int min;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        min = i;
+        for (int j = i + 1; j < n; ++j)
+            if (arr[j] < arr[min])
+                min = j;
+        tmp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = tmp;
+    }
+    
+    fstream archivo(nombre, ios::out | ios::app);
+    for(int i = 0; i < n; i++)
+        archivo << arr[i] << endl;
+    archivo << "---------------------------------\n";
 }
